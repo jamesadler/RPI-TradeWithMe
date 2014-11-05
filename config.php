5d606b1e0b7af55b7656e41cf6b8de13f3d545d5
jamesadler <jfa@jamesfadler.com>
2014-11-05 18:22:20 -0500

    backup
    updating all files from florist

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+
+$user = "root";
+$pass = "";
+
+$dbconn = new PDO('mysql:host=localhost;dbname=TradeWithMe',$user,$pass);
+
+
+?>
---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once("config.php");
+
+
+function login() {
+	echo "login";
+}
+
+
